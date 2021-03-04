8d75350961035fd974db3b00197ca20e774c0285
nmhummel <natalie.hummel@gmail.com>
2021-03-03 18:57:37 -0600

    commit tree 19b057e5ef74520b3762d20b9bbb6c0f0a418803

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,21 @@
 // Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <input type="password" onKeyUp={this.handlePassword}
+                />
+            </div>
+        )
+    }
+
+    handlePassword = () => console.log("Entering password...")
+
+
+
+}
+
+export default Keypad;
